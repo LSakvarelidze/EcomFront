d60330f6fe8d67f83e587947e566f9193e0fbf3b
Levan Sakvarelidze <l.sakvarelidze@hotmail.com>
2022-01-24 17:07:35 +0400

    Login Component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,21 @@
 import react from "react";
-import styles from './Login.module.css'
+import styles from "./Login.module.css";
 
 const Login = () => {
-  return <div>I am login</div>;
+  return (
+      <div className={styles.loginWindow}>
+        <div className={styles.notify}>Sample Text will be shown on error!</div>
+        <form>
+          <label>Username: </label>
+          <input type="text"></input>
+          <label>Password: </label>
+          <input type="password"></input>
+          <button>Login</button>
+          <button>Forgot password?</button>
+          <button>Register</button>
+        </form>
+      </div>
+  );
 };
 
 export default Login;
